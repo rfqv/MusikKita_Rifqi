02f235ee1a1c11fbdc7360abe2a0acc4c897fd98
M Rifqi Virgiansyah <rifqivirgiansyah@gmail.com>
2022-11-07 11:21:28 +0700

    update2

--- app/src/main/java/mrifqiv/if1b/musikkita/MainActivity.java
+++ app/src/main/java/mrifqiv/if1b/musikkita/MainActivity.java
@@ -1,14 +1,17 @@
 package mrifqiv.if1b.musikkita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bnvSpotify;
@@ -20,6 +23,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+        bukaFragment(new MusikFragment());
+        getSupportActionBar().setTitle("Musik");
+        bnvSpotify.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fr;
+                switch (item.getItemId()){
+                    case R.id.menu_musik:
+                        bukaFragment(new MusikFragment());
+                        getSupportActionBar().setTitle("Musik");
+                        return true;
+                    case R.id.menu_album:
+                        bukaFragment(new AlbumFragment());
+                        getSupportActionBar().setTitle("Album");
+                        return true;
+                    case R.id.menu_artis:
+                        bukaFragment(new ArtisFragment());
+                        getSupportActionBar().setTitle("Artis");
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     private void initView(){
